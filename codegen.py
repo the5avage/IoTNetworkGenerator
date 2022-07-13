9b334c05fcafa5aa4b5f013598f52a3187dba619
René Heldmaier <rene.heldmaier@gmail.com>
2022-07-13 13:10:35 +0200

    merge ble and mqtt codegenerator

--- codegen.py
+++ codegen.py
@@ -1,52 +1,83 @@
+import code
 import json
 import shutil
 import os
 import uuid
 import jinja2
 import copy
+import sys
 
-templateEnv = jinja2.Environment(
-    loader=jinja2.FileSystemLoader('TemplateServerBLE'),
-    trim_blocks=True, lstrip_blocks=True)
+def usage():
+    print( sys.argv[0])
+    print()
+    print("codegen.py <Json config file> [destination directory]")
+    sys.exit(1)
 
-serviceUUID = str(uuid.uuid4())
+if len(sys.argv) < 2:
+    usage()
 
-configFile = "TestProject/ExampleConfig.json"
-destinationDir = "TestProject"
+configFile = sys.argv[1]
+destinationDir = os.getcwd()
+if len(sys.argv) >= 3:
+    destinationDir = sys.argv[2]
+
+codeGenDir = os.path.dirname(os.path.realpath(sys.argv[0]))
+serverDir = os.path.join(destinationDir,"Server")
+if os.path.isdir(serverDir):
+    shutil.rmtree(serverDir)
+
+def copyTemplateFile(sourceFilename, destFilpath, templateParam, templateEnv):
+    content = templateEnv.get_template(sourceFilename).render(templateParam)
+
+    with open(destFilpath, "w+") as f:
+        f.write(content)
+
+def generateBLEServer():
+    print("Generate BLE server at: "  + serverDir)
+    serverTemplateDir = os.path.join(codeGenDir, "TemplateServerBLE")
+    templateEnv = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(serverTemplateDir),
+        trim_blocks=True, lstrip_blocks=True)
+
+    os.mkdir(serverDir)
+
+    shutil.copyfile(os.path.join(serverTemplateDir,"Characteristic.h"), os.path.join(serverDir, "Characteristic.h"))
+    copyTemplateFile("Server.ino", os.path.join(serverDir, "Server.ino"), config, templateEnv)
+    with open(os.path.join(serverDir, ".gitignore"), "w+") as f:
+        f.write("*\n")
 
 with open(configFile, "r") as f:
     config = json.load(f)
 
-config["service_uuid"] = serviceUUID
+protocol = config["communication_protocol"]["name"]
 
-serverDir = os.path.join(destinationDir,"Server")
-if os.path.isdir(serverDir):
-    shutil.rmtree(serverDir)
-os.mkdir(serverDir)
+templateDir = ""
+if protocol == "BLE":
+    generateBLEServer()
+    templateDir = os.path.join(codeGenDir, "TemplateNodeBLE")
+    serviceUUID = str(uuid.uuid4())
+    config["service_uuid"] = serviceUUID
+    for node in config["nodes"]:
+        for v in node.get("variables", []):
+            v["uuid"] = uuid.uuid4()
+elif protocol == "MQTT":
+    templateDir = os.path.join(codeGenDir, "TemplateNodeMQTT")
 
-shutil.copyfile(os.path.join("TemplateServerBLE","Characteristic.h"), os.path.join(serverDir, "Characteristic.h"))
-
-for node in config["nodes"]:
-    for v in node.get("variables", []):
-        v["uuid"] = uuid.uuid4()
-
-serverINO = templateEnv.get_template('Server.ino').render(config)
-
-with open(os.path.join(serverDir, "Server.ino"), "w+") as f:
-    f.write(serverINO)
-
-with open(os.path.join(serverDir, ".gitignore"), "w+") as f:
-    f.write("*\n")
-
-templateDir = "TemplateNodeBLE"
 templateDirSrc = os.path.join(templateDir, "src")
-templateFilesNoModify = ["RemoteValue.h", "Util.h", "Util.cpp", "Internal.h"]
+templateFilesSrc = [
+    "RemoteValue.h",
+    "Util.h", "Util.cpp",
+    "Internal.h", "Internal.cpp",
+    "RemoteValues.h", "RemoteValues.cpp",
+    "GenCode.h"]
+
 templateEnv = jinja2.Environment(
     loader=jinja2.FileSystemLoader([templateDir, templateDirSrc]),
     trim_blocks=True, lstrip_blocks=True)
 
 def generateNode(thisNode):
-    otherNodes = copy.deepcopy(list(filter(lambda n: n["name"] in thisNode.get("using", []), config["nodes"])))
+    nodeConfig = copy.deepcopy(config)
+    otherNodes = list(filter(lambda n: n["name"] in thisNode.get("using", []), nodeConfig["nodes"]))
     for ob in thisNode.get("observe", []):
         obInfo = ob.split("::")
         observedNode = next(filter(lambda n: n["name"] == obInfo[0], otherNodes))
@@ -54,6 +85,7 @@ def generateNode(thisNode):
         observedVariable["isObserved"] = True
 
     nodeDir = os.path.join(destinationDir, node["name"])
+    print(f'generate Node: {node["name"]} at: ' + nodeDir)
 
     if not os.path.isdir(nodeDir):
         os.mkdir(nodeDir)
@@ -63,35 +95,13 @@ def generateNode(thisNode):
     if not os.path.isdir(nodeDirSrc):
         os.mkdir(nodeDirSrc)
 
-    for t in templateFilesNoModify:
-        shutil.copy(os.path.join(templateDirSrc, t), nodeDirSrc)
-
     shutil.copy(os.path.join(templateDir, "Template.ino"), os.path.join(nodeDir, node["name"] + ".ino"))
 
-    templateParam = {
-        'otherNodes' : otherNodes,
-        'thisNode' : thisNode,
-        'service_uuid' : serviceUUID
-    }
-    InternalCpp = templateEnv.get_template('Internal.cpp').render(templateParam)
+    nodeConfig['otherNodes'] = otherNodes
+    nodeConfig['thisNode'] = thisNode
 
-    with open(os.path.join(nodeDirSrc, "Internal.cpp"), "w+") as f:
-        f.write(InternalCpp)
-
-    RemoteValuesH = templateEnv.get_template("RemoteValues.h").render(templateParam)
-
-    with open(os.path.join(nodeDirSrc, "RemoteValues.h"), "w+") as f:
-        f.write(RemoteValuesH)
-
-    RemoteValuesCpp = templateEnv.get_template("RemoteValues.cpp").render(templateParam)
-
-    with open(os.path.join(nodeDirSrc, "RemoteValues.cpp"), "w+") as f:
-        f.write(RemoteValuesCpp)
-
-    GenCodeH = templateEnv.get_template("GenCode.h").render(templateParam)
-
-    with open(os.path.join(nodeDirSrc, "GenCode.h"), "w+") as f:
-        f.write(GenCodeH)
+    for template in templateFilesSrc:
+        copyTemplateFile(template, os.path.join(nodeDirSrc, template), nodeConfig, templateEnv)
 
     customCodeH = os.path.join(nodeDir, "CustomCode.h")
     customCodeHTemplate = os.path.join(templateDir, "CustomCode.h")
@@ -101,16 +111,13 @@ def generateNode(thisNode):
         shutil.copy(customCodeHTemplate, customCodeH)
 
     customCodeCppPath = os.path.join(nodeDir, "CustomCode.cpp")
-    customCodeCpp = templateEnv.get_template('CustomCode.cpp').render(templateParam)
     if os.path.exists(customCodeCppPath):
         customCodeCppPath = os.path.join(nodeDir, "CustomCode.cpp_template")
 
-    with open(customCodeCppPath, "w+") as f:
-        f.write(customCodeCpp)
+    copyTemplateFile("CustomCode.cpp", customCodeCppPath, nodeConfig, templateEnv)
 
     with open(os.path.join(nodeDir, ".gitignore"), "w+") as f:
         f.write(f'/src\n{node["name"]}.ino\n*_template\n')
 
 for node in config["nodes"]:
-    print(f'generate Node: {node["name"]}')
     generateNode(node)
